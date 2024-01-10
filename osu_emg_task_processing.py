0f70f83273c387d4d21b3ba2b0b4bea3d39a301f
Kayla Russell-Bertucci <k.russell.bertucci@gmail.com>
2024-01-09 20:36:10 -0500

    Finished comments

--- osu_emg_task_processing.py
+++ osu_emg_task_processing.py
@@ -1,13 +1,14 @@
-#OSU TASK EMG - Kayla Russell-Bertucci (last modified: Jan 4, 2023)
+#OSU TASK EMG - Kayla Russell-Bertucci (last modified: Jan 9, 2023)
 # INPUTS
-#   1. Subject Number
-#   2. Task files
-#   3. import MVC max from emg_mvc_processing excel output
+#   = 1. Subject Number
+#   = 2. Folder names containing MVC files and trial files
+# NOTE: check that files are in the correct folders with the correct names before running 
 # PURPOSE
-#   filters, removes bias, & full wave rectify for each muscle of task
+#   filters, removes bias, & full wave rectify for each muscle of task and MVC trials
 #   normalize amplitude to peak MVC
-# OUTPUT = 1. Processed task trials (individual & averaged)
-#        = 2. %MVC peak for individual muscles in each mouse sensitivity and difficulty per participant
+# OUTPUT 
+#   = 1. MVC max values for each muscle exports to a csv
+#   = 2. %MVC peak for individual muscles in each mouse sensitivity and difficulty per participant exported to csv
 #
 # TRIAL NAMING CONVENTIONS
 # eh - easy diff high sens 
@@ -23,13 +24,13 @@ import numpy as np
 import glob
 import emg_signal_processing_functions as emg_sp
 
-# set subject number
+# CHANGE SUBJECT NUMBER BEFORE RUNNING SCRIPT
 sub_num = "S05"
 trial_folder = f"C:/Users/kruss/OneDrive - University of Waterloo/Documents/OSU/Data/{sub_num}/Data_Raw/Trial_EMG/Trial_EMG_Files" #sets path for files
-# TODO: Step 1 Create new Folder similar to subject folder but for the MVC path.
+
 mvc_folder = f"C:/Users/kruss/OneDrive - University of Waterloo/Documents/OSU/Data/{sub_num}/Data_Raw/Trial_EMG/MVC"
 
-# dictionary with each condition and empty values - the values will be updated with the maxes
+# dictionary with each condition and empty values - the values will be updated with the maxes from line 100
 condition_maxs = {
     "EASY_PREF": [],
     "EASY_HIGH": [],
@@ -38,7 +39,7 @@ condition_maxs = {
     "HARD_HIGH": [],
     "HARD_LOW": [],
 }
-
+# dictionary containing each condition with empty max values - max values will be updated in line 88
 normalized_maxs = {
     "EASY_PREF": [],
     "EASY_HIGH": [],
@@ -63,14 +64,13 @@ muscles = {
     "ECRB": 12
 } 
 
+# empty array to append individual mvc values in column order
 muscle_mvc_max_arr = []
 for muscle, col in muscles.items():
-    # TODO Step 2.
-    # Create path to muscle trial 1
-    # Read muscle MVC file
-    # process muscle
-    # get muscle max for trial1
+    # sets an mvc to -1 to be overwritten when max is calculated, or to indicate a trial is missing
     muscle_mvc_max = -1
+    # iterates between trials 1 and 2, indicated by the suffix of the trial
+    # NOTE: THIS WILL NOT WORK ON TRIALS WITH '3' OR '2A', RENAME FILES PRIOR TO RUNNING THE SCRIPT
     for attempt in [1,2]: 
         # path mvc trial
         mvc_trial_path = f"{mvc_folder}\{sub_num}_MVC_{muscle}_{attempt}_a.tsv" 
@@ -78,39 +78,61 @@ for muscle, col in muscles.items():
         mvc_df = pd.read_csv(mvc_trial_path, sep='\t', header=13)
         # obtains the indexed signal of the 2nd trial
         mvc_df_signal = mvc_df[f"EMG {col}"].to_numpy()
+        # processes the signal 
         processed_mvc_signal = emg_sp.process_muscle(mvc_df_signal)
         # gets max value from processed signal
         mvc_trial_max = np.max(processed_mvc_signal)
+        # temporarily stores the max value between previous or current trial. previous can be muscle_mvc_max
         muscle_mvc_max = max(muscle_mvc_max, mvc_trial_max)
+    # once max values are stored to muscle_mvc_max, they are appended to muscle_mvc_max_arr before the loop moves on to the next muscle 
     muscle_mvc_max_arr.append(muscle_mvc_max)
 
+    # iterates through dictionary 'normalized_maxes' by condition which is paired to norm_maxs
     for condition, norm_maxs in normalized_maxs.items():
+        # sets folder prefix to obtain files
         folder_prefix = f'd_{sub_num}_{condition}*.tsv'
+        # groups trial paths based on their folder prefix which contains the condition name
         condition_trial_paths = glob.glob(f'{trial_folder}/{folder_prefix}')
+        # sets max to -1. max will always be greater than -1. if csv contains -1, there is an issue or trial does not exist
         condition_max = -1
         
+        # iterates through trial count (condition_trial) of the grouped files in condition_trial_paths 
         for condition_trial in condition_trial_paths:
+            # creates a data frame from csv containing trial data
             condition_df = pd.read_csv(condition_trial, sep='\t', header=13)
+            # converts condition_df csv to a numpy file
             condition_signal = condition_df[f"EMG {col}"].to_numpy()
+            # process trial signal using signal processing function (bandpass, fwr)
             processed_signal = emg_sp.process_muscle(condition_signal)
+            # obtains maximal value from the processed signal
             condition_trial_max = np.max(processed_signal)
-            condition_max = max(condition_max, condition_trial_max) #takes the max value of the condition between prec 
-
+            # temporarily stores the max of the current condiion 
+            condition_max = max(condition_max, condition_trial_max)  
+        # appends the max value from condition_max to the empty value in condition_maxs dictionary
         condition_maxs[condition].append(condition_max)
+        # normalizes the condiion maxes to the mvc max value of each muscle if the condition max is not -1
         normalized_max = (condition_max/muscle_mvc_max) if condition_max != -1 else -1
+        # appends the normalized max to the value in normalized_max dictionary
         norm_maxs.append(normalized_max)
         
 
-#export mvc maxes to csv 
+# EXPORT VALUES TO CSV
+
+#  create an array that contains the mvc max values
 mvc_muscle_max_arr = np.array(muscle_mvc_max_arr)
+# adds a column of zeroes
 mvc_muscle_max_arr = np.expand_dims(mvc_muscle_max_arr, axis=0)
+# converts the mvc_muscle_max_arr to a dataframe containing the muscles as column names
 mvc_muscle_csv = pd.DataFrame(data=mvc_muscle_max_arr, columns=muscles.keys())
+# converts the data frame to a csv format
 mvc_muscle_csv.to_csv(f"{mvc_folder}/{sub_num}_MVC_values.csv")
 
+# creates a data frame with the un-normalized condition max values
 df = pd.DataFrame.from_dict(condition_maxs, orient='index', columns=muscles.keys())
 df.to_csv(f'{trial_folder}/{sub_num}_condition_maxs.csv')
 print(f"subject max emg values have been written to {trial_folder}/{sub_num}_condition_maxs.csv'")
 
-df = pd.DataFrame.from_dict(normalized_maxs, orient='index', columns=muscles.keys())
-df.to_csv(f'{trial_folder}/{sub_num}_normalized_condition_maxs.csv')
+# creates a data frame with the normalized condition max values
+df_norm = pd.DataFrame.from_dict(normalized_maxs, orient='index', columns=muscles.keys())
+df_norm.to_csv(f'{trial_folder}/{sub_num}_normalized_condition_maxs.csv')
 print(f"subject max emg values have been written to {trial_folder}/{sub_num}_normalized_condition_maxs.csv'")
